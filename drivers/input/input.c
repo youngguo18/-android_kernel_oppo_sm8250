19442ca85e9ecd645e4c0ef47af7123c1e20536c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-02 10:49:35 +0800

    KernelSU: manual filesystem hooks + selinux hook https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    F-19-F/android_kernel_oneplus_msm8998@1042d56

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -371,6 +371,10 @@ static int input_get_disposition(struct input_dev *dev,
 extern void __attribute__((weak)) oplus_sync_saupwk_event(unsigned int , unsigned int , int);
 #endif /* OPLUS_FEATURE_SAUPWK */
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif
+
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
@@ -380,6 +384,10 @@ static void input_handle_event(struct input_dev *dev,
 	if(oplus_sync_saupwk_event)
         oplus_sync_saupwk_event(type, code, value);
 #endif /* OPLUS_FEATURE_SAUPWK */
+
+	#ifdef CONFIG_KSU
+		ksu_handle_input_handle_event(&type, &code, &value);
+	#endif
 
 	if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)
 		add_input_randomness(type, code, value);
