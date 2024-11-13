cf510cd17211095352df5a2f51e4aa9c7f241356
youngguo18 <guoamao@163.com>
2024-11-13 11:27:08 +0800

    fix flash anykernel.zip error

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -15,8 +15,8 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=boot;
-is_slot_device=1;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
